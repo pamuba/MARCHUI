6ba73e7a50ee1c0587e681e981a649876883b1e0
pamuba <partha.bora24@gmail.com>
2021-03-30 20:23:53 +0530

    51

--- canvas2.js
+++ canvas2.js
@@ -28,13 +28,41 @@ var c = canvas.getContext('2d');
 // c.stroke();
 
 
-for(let i=0;i<100;i++){
-    var x = Math.random() * window.innerHeight;
-    var y = Math.random() * window.innerWidth;
+// for(let i=0;i<100;i++){
+//     var x = Math.random() * window.innerHeight;
+//     var y = Math.random() * window.innerWidth;
+//     c.beginPath();
+//     c.arc(x,y,30,0, Math.PI*2, false)
+//     c.strokeStyle = "blue"
+//     c.fillStyle = "#"+i*3;
+//     c.fill()
+//     c.stroke();
+// }
+var x = Math.random() * innerWidth; 
+var y = Math.random() * innerHeight;
+var dx = (Math.random()-0.5) * 10
+var dy = (Math.random()-0.5) * 10
+var radius = 30;
+function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0,0, innerWidth, innerHeight)
+
     c.beginPath();
-    c.arc(x,y,30,0, Math.PI*2, false)
+    c.arc(x,y,radius, 0, Math.PI*2, false);
     c.strokeStyle = "blue"
-    c.fillStyle = "gold"
+    c.fillStyle = "gold";
     c.fill()
     c.stroke();
+
+    if(x+radius > innerWidth || x-radius < 0){
+        dx = -dx;
+    }
+    x+=dx
+
+    if(y+radius > innerHeight || y-radius < 0){
+        dy = -dy;
+    }
+    y+=dy
 }
+
+animate();

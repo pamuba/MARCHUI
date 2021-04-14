0b82838e5652a6d8f83b2b99c6c860e9d686825b
pamuba <partha.bora24@gmail.com>
2021-04-14 23:23:57 +0530

    1333

---
+++ JS/array.js
@@ -0,0 +1,36 @@
+//push
+//pop
+//shift
+//unshift
+//splice()
+//concat()
+//Array.from()
+//iterable "hello" [1,2,3]
+
+//new Array()
+
+// what array means?
+
+// is an ordered list of values
+// array means data struture that store a list of elements....
+// in JS we can define array as a function right?
+
+// isnt array a user defined variable
+// a = [1,2,3]
+
+// b = [a, 11,22,22,3,5,11]
+
+// console.log(b[a[0]])
+// console.log(b[0][0])
+
+// var fruits = ["banana" ,"apple", "grapes"]
+// console.log(fruits.sort())
+// console.log(fruits)
+
+
+// var nums = [1,2,11,23,5]
+// console.log(nums.sort())
+// nums.sort((a,b) => b - a)
+// console.log(nums)
+
+//map , reduce , filter 

0c80981c7e268a345856c9a870d194e521299ba4
pamuba <partha.bora24@gmail.com>
2021-05-07 20:08:51 +0530

    281

--- newapp/src/app/first/first.component.ts
+++ newapp/src/app/first/first.component.ts
@@ -2,14 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first',
-  templateUrl: './first.component.html',
-  styleUrls: ['./first.component.css']
+  template: `<h1>Inline HTML</h1>
+             <p>Hello {{name}}</p>
+             <p>{{name.length}}</p>
+             <p>{{name.toUpperCase()}}</p>
+             <p>{{hello()}}</p>`,
+  styles: []
 })
 export class FirstComponent implements OnInit {
 
+
+  public name = "John"
   constructor() { }
 
   ngOnInit(): void {
+  }
+  hello(){
+    return "Hello " + this.name;
   }
 
 }
